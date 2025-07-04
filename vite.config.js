bfbef7e2bbd77ef6154c6cca61819fc4f58215df
amitabh-anandpd <amitabh.anandpd@gmail.com>
2025-07-04 14:59:09 +0530

    Initial Commit

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        auth: resolve(__dirname, 'auth.html'),
+        dailyQuest: resolve(__dirname, 'daily-quest.html'),
+        dashboard: resolve(__dirname, 'dashboard.html'),
+        initialScreening: resolve(__dirname, 'initial-screening.html'),
+        leaderboard: resolve(__dirname, 'leaderboard.html'),
+        onboarding: resolve(__dirname, 'onboarding.html'),
+        studyGuide: resolve(__dirname, 'study-guide.html'),
+        profile: resolve(__dirname, 'profile.html'),
+        testCenter: resolve(__dirname, 'test-center.html'),
+        testConceptual: resolve(__dirname, 'test-conceptual.html'),
+        testMcq: resolve(__dirname, 'test-mcq.html'),
+        testSpeed: resolve(__dirname, 'test-speed.html'),
+      },
+    },
+    outDir: 'dist',
+  },
+});
